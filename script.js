82be53279acf5b9b2a03793743fb2336316df6a8
salamelag <132400260+salamelag@users.noreply.github.com>
2026-05-16 00:45:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var map = L.map('map').setView([-35.0, -64.0], 6);
+// Iniciamos la cámara más lejos (Zoom 4) y centrada entre Argentina y Brasil
+var map = L.map('map').setView([-25.0, -55.0], 4);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// ----------------- MARCADOR 1: LAGUNA MAR CHIQUITA -----------------
+// ----------------- MARCADOR 1: LAGUNA MAR CHIQUITA (CÓRDOBA) -----------------
 var coordsMarChiquita = [-30.600242, -62.870913];
 var linkImagenMar = 'https://tr.rbxcdn.com/180DAY-8c528bd4c92002faf069c7f4f966f9f9/256/256/Image/Webp/noFilter';
 var linkJuegoMar = 'https://www.roblox.com/games/119851378620864/25-REMASTER';
@@ -13,7 +14,7 @@ var marcadorMar = L.marker(coordsMarChiquita).addTo(map);
 marcadorMar.bindTooltip("Laguna Mar Chiquita <br> Link del juego: " + linkJuegoMar, { direction: 'top', offset: [0, -10] });
 marcadorMar.bindPopup("<a href='" + linkJuegoMar + "' target='_blank'><b>¡Haz clic aquí para jugar 25 REMASTER!</b></a>");
 
-// ----------------- MARCADOR 2: CAMPO DE MAYO -----------------
+// ----------------- MARCADOR 2: CAMPO DE MAYO (BUENOS AIRES) -----------------
 var coordsCampoMayo = [-34.533805, -58.649166];
 var linkImagenCampo = 'https://tr.rbxcdn.com/180DAY-cdfd2b3c913f59789ac50bda58fa8e97/256/256/Image/Webp/noFilter';
 var linkJuegoCampo = 'https://www.roblox.com/games/86744432712071/Argentine-Army';
@@ -22,38 +23,48 @@ var marcadorCampo = L.marker(coordsCampoMayo).addTo(map);
 marcadorCampo.bindTooltip("Campo de Mayo <br> Link del juego: " + linkJuegoCampo, { direction: 'top', offset: [0, -10] });
 marcadorCampo.bindPopup("<a href='" + linkJuegoCampo + "' target='_blank'><b>¡Haz clic aquí para jugar Argentine Army!</b></a>");
 
+// ----------------- MARCADOR 3: BRASÍLIA (BRASIL) -----------------
+var coordsBrasilia = [-15.778361, -47.905083]; 
+var linkImagenBrasilia = 'https://tr.rbxcdn.com/180DAY-40a3b8aacb25617525f5903f172f4db8/256/256/Image/Webp/noFilter';
+var linkJuegoBrasilia = 'https://www.roblox.com/games/2069320852/Ex-rcito-Brasileiro-EB';
+
+var marcadorBrasilia = L.marker(coordsBrasilia).addTo(map);
+marcadorBrasilia.bindTooltip("Brasília <br> Link del juego: " + linkJuegoBrasilia, { direction: 'top', offset: [0, -10] });
+marcadorBrasilia.bindPopup("<a href='" + linkJuegoBrasilia + "' target='_blank'><b>¡Haz clic aquí para jugar Exército Brasileiro!</b></a>");
+
+
+// ----------------- ACTUALIZAR TAMAÑO (TODOS LOS ICONOS) -----------------
 function actualizarTamanoIcono() {
     var zoomActual = map.getZoom();
     var nuevoTamano = Math.max(30, zoomActual * 6); 
     
-    var iconoMar = L.icon({
-        iconUrl: linkImagenMar,   
-        iconSize: [nuevoTamano, nuevoTamano],      
-        iconAnchor: [nuevoTamano / 2, nuevoTamano / 2], 
-        className: 'icono-con-borde' 
-    });
-    marcadorMar.setIcon(iconoMar);
+    // Agrupamos todos en una lista para hacer el código más corto y ordenado
+    var listaMarcadores = [
+        { obj: marcadorMar, url: linkImagenMar },
+        { obj: marcadorCampo, url: linkImagenCampo },
+        { obj: marcadorBrasilia, url: linkImagenBrasilia }
+    ];
 
-    var iconoCampo = L.icon({
-        iconUrl: linkImagenCampo,   
-        iconSize: [nuevoTamano, nuevoTamano],      
-        iconAnchor: [nuevoTamano / 2, nuevoTamano / 2], 
-        className: 'icono-con-borde' 
+    listaMarcadores.forEach(function(item) {
+        var icono = L.icon({
+            iconUrl: item.url,   
+            iconSize: [nuevoTamano, nuevoTamano],      
+            iconAnchor: [nuevoTamano / 2, nuevoTamano / 2], 
+            className: 'icono-con-borde' 
+        });
+        item.obj.setIcon(icono);
     });
-    marcadorCampo.setIcon(iconoCampo);
 }
 
 map.on('zoomend', actualizarTamanoIcono);
 actualizarTamanoIcono();
 
-// ----------------- LEER ARCHIVO Y DIBUJAR PROVINCIAS -----------------
+
+// ----------------- LEER ARCHIVO Y DIBUJAR PROVINCIAS (ARGENTINA) -----------------
 fetch('provincias.geojson')
-    .then(function(respuesta) {
-        return respuesta.json();
-    })
+    .then(function(respuesta) { return respuesta.json(); })
     .then(function(datosProvincias) {
         L.geoJSON(datosProvincias, {
-        
             filter: function(feature) {
                 return feature.properties.nombre === 'Córdoba' || feature.properties.nombre === 'Buenos Aires';
             },
@@ -61,12 +72,26 @@ fetch('provincias.geojson')
                 if (feature.properties.nombre === 'Córdoba') {
                     return { color: '#00bfff', weight: 3, fillColor: '#b0e0e6', fillOpacity: 0.25 };
                 } else if (feature.properties.nombre === 'Buenos Aires') {
-                    return { color: '#00008B', weight: 3, fillColor: '#0067cd', fillOpacity: 0.25 };
+                    return { color: '#00008B', weight: 3, fillColor: '#0000CD', fillOpacity: 0.25 };
                 }
             }
-            
         }).addTo(map);
     })
-    .catch(function(error) {
-        console.error("Hubo un error cargando el archivo geojson:", error);
-    });
+    .catch(function(error) { console.error("Error cargando provincias:", error); });
+
+
+// ----------------- LEER ARCHIVO Y DIBUJAR BRASÍLIA (BRASIL) -----------------
+fetch('https://raw.githubusercontent.com/tbrugz/geodata-br/refs/heads/master/geojson/geojs-100-mun.json')
+    .then(function(respuesta) { return respuesta.json(); })
+    .then(function(datosBrasil) {
+        L.geoJSON(datosBrasil, {
+            filter: function(feature) {
+                var nombre = feature.properties.name || feature.properties.NM_MUN || feature.properties.description;
+                return nombre === 'Brasília' || nombre === 'Distrito Federal';
+            },
+            style: function(feature) {
+                return { color: '#006400', weight: 3, fillColor: '#32CD32', fillOpacity: 0.25 };
+            }
+        }).addTo(map);
+    })
+    .catch(function(error) { console.error("Error cargando geojson de Brasil:", error); });
