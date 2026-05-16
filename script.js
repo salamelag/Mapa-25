85e6e7757124f2474c9766d805f73d44408d5d69
salamelag <132400260+salamelag@users.noreply.github.com>
2026-05-15 23:13:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
-// 1. Tu mapa base
-var map = L.map('map').setView([-34.6, -58.4], 8); // Acerqué un poco el zoom (8)
+var map = L.map('map').setView([-32.1, -63.6], 7);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-var coordenadasTerritorio = [
-    [-34.8, -58.0],
-    [-34.8, -57.6],
-    [-35.2, -57.6],
-    [-35.2, -58.0]
-];
-
-var zonaMilitar = L.polygon(coordenadasTerritorio, {
-    color: '#ff0000',
-    weight: 3,
-    fillColor: '#aa0000',
-    fillOpacity: 0.4
-}).addTo(map);
-
-zonaMilitar.bindPopup("<b>Zona Restringida</b><br>Isla de Operaciones");
-
-// ==========================================
-// 3. ÍCONOS MILITARES PERSONALIZADOS
-// ==========================================
-var iconoMilsim = L.icon({
-    // Cambiá esta URL por el link a tu imagen PNG/SVG (ej: 'icono-tactico.png' si lo subís a tu GitHub)
-    iconUrl: 'https://cdn-icons-png.flaticon.com/512/1004/1004305.png', 
-    iconSize: [40, 40],    // Tamaño del ícono [ancho, alto]
-    iconAnchor: [20, 40],  // Punto exacto del ícono que toca la coordenada del mapa
-    popupAnchor: [0, -40]  // Dónde aparece el popup en relación al ícono
-});
-
-var basePrincipal = L.marker([-35.0, -57.8], {icon: iconoMilsim}).addTo(map);
-
-basePrincipal.bindPopup("<b>HQ Principal</b><br>Unidad Táctica Desplegada.");
+fetch('https://apis.datos.gob.ar/georef/api/v2.0/provincias.geojson?geometria=completa')
+    .then(res => res.json())
+    .then(data => {
+        var cordoba = data.features.find(f => f.properties.nombre.toLowerCase().includes('cór'));
+        
+        L.geoJSON(cordoba, {
+            style: {
+                color: '#00bfff',     
+                weight: 3,
+                fillColor: '#b0e0e6', 
+                fillOpacity: 0.25     
+            }
+        }).addTo(map);
+    });
