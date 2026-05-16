c88b4fb911d46f976fab74e451a149b5488cda9f
salamelag <132400260+salamelag@users.noreply.github.com>
2026-05-15 22:55:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([-34.6, -58.4], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
