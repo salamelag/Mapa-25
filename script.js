ff463dc13f40561cb7ddd2d400822932e02f2a4b
salamelag <132400260+salamelag@users.noreply.github.com>
2026-05-15 23:19:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-fetch('https://apis.datos.gob.ar/georef/api/v2.0/provincias.geojson?geometria=completa')
-    .then(res => res.json())
-    .then(data => {
-        var cordoba = data.features.find(f => f.properties.nombre.toLowerCase().includes('cór'));
-        
-        L.geoJSON(cordoba, {
-            style: {
-                color: '#00bfff',     
-                weight: 3,
-                fillColor: '#b0e0e6', 
-                fillOpacity: 0.25     
-            }
-        }).addTo(map);
-    });
+// Límite geométrico de Córdoba (sin fetch, evita CORS y 404)
+var contornoCordoba = [
+    [-29.68, -65.62],
+    [-29.70, -62.95],
+    [-30.26, -61.93],
+    [-31.65, -62.15],
+    [-34.35, -62.25],
+    [-35.00, -63.38],
+    [-35.00, -65.00],
+    [-32.50, -65.10],
+    [-31.90, -65.48],
+    [-30.80, -65.45]
+];
+
+L.polygon(contornoCordoba, {
+    color: '#00bfff',     
+    weight: 3,
+    fillColor: '#b0e0e6', 
+    fillOpacity: 0.25     
+}).addTo(map);
